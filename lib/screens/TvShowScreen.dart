de1aac5527aca0c03da817b2bfd9417516605292
kei-kusanagi <kei_kusanagi@outlook.com>
2023-02-02 14:13:02 -0600

    add flutter_slidable: ^2.0.0

--- lib/screens/TvShowScreen.dart
+++ lib/screens/TvShowScreen.dart
@@ -1,5 +1,5 @@
 import 'dart:ffi';
-
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:flutter/material.dart';
 
 class TvShow extends StatefulWidget {
@@ -12,7 +12,7 @@ class TvShow extends StatefulWidget {
 class _TvShowState extends State<TvShow> {
 
 
-  List<TVshow> _TVShows =[
+  final List<TVshow> _TVShows =[
     TVshow('The Last of Us', 'tt3581920', 9.3, 'https://m.media-amazon.com/images/M/MV5BZGUzYTI3M2EtZmM0Yy00NGUyLWI4ODEtN2Q3ZGJlYzhhZjU3XkEyXkFqcGdeQXVyNTM0OTY1OQ@@._V1_UX67_CR0,0,67,98_AL_.jpg'),
     TVshow('El menu', 'tt9764362', 7.2,'https://m.media-amazon.com/images/M/MV5BMzdjNjI5MmYtODhiNS00NTcyLWEzZmUtYzVmODM5YzExNDE3XkEyXkFqcGdeQXVyMTAyMjQ3NzQ1._V1_QL75_UX280_CR0,3,280,414_.jpg'),
     TVshow('TOP GUN MAVERICK', 'tt1745960', 8.3,'https://m.media-amazon.com/images/M/MV5BZWYzOGEwNTgtNWU3NS00ZTQ0LWJkODUtMmVhMjIwMjA1ZmQwXkEyXkFqcGdeQXVyMjkwOTAyMDU@._V1_QL75_UX280_CR0,0,280,414_.jpg'),
@@ -32,12 +32,34 @@ class _TvShowState extends State<TvShow> {
         body: ListView.builder(
           itemCount: _TVShows.length,
             itemBuilder: (context, index){
-              return Dismissible(
+              return Slidable(
                 key: Key(_TVShows[index].toString()),
-                direction: DismissDirection.endToStart,
-                onDismissed: (direction) {
-                  print('BORRAR');
-                },
+                // direction: DismissDirection.endToStart,
+                // onDismissed: (direction) {
+                //   this._deleteTVshow(context, _TVShows[index]);
+                // },
+                startActionPane: ActionPane(
+                  motion: const ScrollMotion(),
+                  dismissible: DismissiblePane(onDismissed: (){},
+                  ), children: [SlidableAction(
+                  onPressed: (BuildContext context) {
+                    print("Se borro");
+                  },
+                  backgroundColor: Color(0xFFFE4A49),
+                  foregroundColor: Colors.white,
+                  icon: Icons.delete,
+                  label: 'Delete',
+                ),
+                  SlidableAction(
+                    onPressed: (BuildContext context) {
+                      print("Se compartio");
+                    },
+                    backgroundColor: Color(0xFF21B7CA),
+                    foregroundColor: Colors.white,
+                    icon: Icons.share,
+                    label: 'Share',
+                  ),],
+                ),
                 child: ListTile(
                   onTap: (){
                     print(_TVShows[index].name);
@@ -67,6 +89,28 @@ class _TvShowState extends State<TvShow> {
 
     );
   }
+  _deleteTVshow(context, TVshow){
+    showDialog(
+      context: context,
+      builder: ( _ ) => AlertDialog(
+        title: Text("Eliminar Show"),
+        content:  Text("¿Esta seguro de querer eliminar a " + TVshow.name + '?'),
+        actions: [
+          TextButton(onPressed: (){
+            Navigator.pop(context);
+          }, child:Text('Cancelar',),),
+          TextButton(onPressed: (){
+            print(TVshow.name);
+            setState(() {
+              _TVShows.remove(TVshow);
+            });
+
+            Navigator.pop(context);
+          }, child:Text('Borrar', style: TextStyle(color: Colors.red),),),
+        ],
+      ),
+    );
+  }
 }
 
 
@@ -84,3 +128,5 @@ class TVshow{
 
     }
 }
+
+void doNothing(BuildContext context) {}
