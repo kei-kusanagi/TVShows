49454ef1af32fc39cd422c1dfa21fc80b1a947d2
kei-kusanagi <kei_kusanagi@outlook.com>
2023-02-03 19:31:10 -0600

    add http api request's

---
+++ lib/api/api.dart
@@ -0,0 +1,9 @@
+import 'package:http/http.dart';
+import 'package:flutter/material.dart';
+
+class Gif {
+  late String name;
+  late String url;
+
+  Gif (this.name, this.url);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,26 @@ import 'package:tv_show/screens/Favorites.dart';
 import 'screens/TvShowScreen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: TvShow(),
-      // home: Scaffold(
-      //   body: PageView(
-      //     children: <Widget>[
-      //       TvShow(),
-      //       Favorites(),
-      //     ]
-      //   )
-      // )
+      debugShowCheckedModeBanner: false,
+      // home: TvShow(),
+      home: Scaffold(
+        body: PageView(
+          physics: BouncingScrollPhysics(),
+          children: <Widget>[
+            TvShow(),
+            Favorites(),
+          ]
+        )
+      )
     );
   }
 }
