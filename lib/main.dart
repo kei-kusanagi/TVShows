607c0de3b8bf3d89e736b28cc4e7cbc5646c50cb
kei-kusanagi <kei_kusanagi@outlook.com>
2023-04-13 21:44:38 -0600

    final UI update

--- lib/main.dart
+++ lib/main.dart
@@ -130,6 +130,11 @@ class _MyAppState extends State<MyApp> {
 
                                           Navigator.of(context).pop();
                                         },
+                                        style: ElevatedButton.styleFrom(
+                                          backgroundColor:
+                                              Provider.of<ScreenModel>(context)
+                                                  .colorTheme,
+                                        ),
                                       ),
                                     ],
                                   );
@@ -159,8 +164,6 @@ class _MyAppState extends State<MyApp> {
               ],
             ),
             bottomNavigationBar: BottomNavigationBar(
-              // backgroundColor: Colors.grey,
-              // unselectedItemColor: Colors.white,
               items: const [
                 BottomNavigationBarItem(
                   icon: Icon(Icons.live_tv_outlined),
@@ -185,66 +188,4 @@ class _MyAppState extends State<MyApp> {
       ),
     );
   }
-
-  // AppBar MainAppbar() {
-  //   Color pickerColor =
-  //       Provider.of<ScreenModel>(context, listen: false).colorTheme;
-  //   Color currentColor =
-  //       Provider.of<ScreenModel>(context, listen: false).colorTheme;
-  //
-  //   void changeColor(Color color) {
-  //     setState(() => pickerColor = color);
-  //   }
-  //
-  //   return AppBar(
-  //     title: Center(
-  //       child: Text(_selectedIndex == 0 ? 'Tv Shows' : 'Favorites'),
-  //     ),
-  //     // backgroundColor: Colors.purple,
-  //     actions: [
-  //       IconButton(
-  //         icon: isDark ? Icon(Icons.sunny) : Icon(Icons.nights_stay),
-  //         onPressed: () {
-  //           setState(() {
-  //             MyApp(
-  //               initialIndex: _selectedIndex,
-  //             ).toggleDarkMode();
-  //           });
-  //         },
-  //       ),
-  //       IconButton(
-  //         onPressed: () {
-  //           showDialog(
-  //             context: context,
-  //             builder: (BuildContext context) {
-  //               return AlertDialog(
-  //                 title: const Text('Pick a color!'),
-  //                 content: SingleChildScrollView(
-  //                   child: BlockPicker(
-  //                     pickerColor: pickerColor,
-  //                     onColorChanged: changeColor,
-  //                   ),
-  //                 ),
-  //                 actions: <Widget>[
-  //                   ElevatedButton(
-  //                     child: const Text('Got it'),
-  //                     onPressed: () {
-  //                       setState(() => currentColor = pickerColor);
-  //                       print(pickerColor);
-  //                       Provider.of<ScreenModel>(context, listen: false)
-  //                           .colorTheme = pickerColor;
-  //
-  //                       Navigator.of(context).pop();
-  //                     },
-  //                   ),
-  //                 ],
-  //               );
-  //             },
-  //           );
-  //         },
-  //         icon: Icon(Icons.color_lens_outlined),
-  //       ),
-  //     ],
-  //   );
-  // }
 }
